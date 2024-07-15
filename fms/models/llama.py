8de1a798e9f6e550b4345d2cba85bf3caa387508
Kaitlyn Nguyen <kaitlyn.nguyen@ibm.com>
2024-07-15 14:52:32 -0400

    remove

--- fms/models/llama.py
+++ fms/models/llama.py
@@ -118,7 +118,7 @@ class LLaMABlock(nn.Module):
         is_causal_mask=False,
         attn_algorithm=None,
     ):
-        print("Entered LLaMABlock forward")
+        # print("Entered LLaMABlock forward")
         # if the cache is not empty, we need to get the kv cache for self and cross attention
         self_attn_past_key_value = past_key_value_state
         # if past_key_value_state is not None:
