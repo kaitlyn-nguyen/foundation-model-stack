bc983c4a748238499693e643e492ffa6c298ab7c
Kaitlyn Nguyen <kaitlyn.nguyen@ibm.com>
2024-07-15 14:47:58 -0400

    revert

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -168,6 +168,28 @@ class FusedQKV(QKV):
             bias=self.use_bias,
         )
 
+    def unfuse_weights(self):
+        result = UnfusedQKV(
+            self.emb_dim,
+            self.nheads,
+            self.kvheads,
+            self.emb_kq_per_head,
+            self.emb_v_per_head,
+            self.use_bias,
+        ).to(self.qkv_fused.weight.device)
+        query, key, value = torch.split(self.qkv_fused.weight, self.splits, dim=0)
+        result.query.weight.copy_(query)
+        result.key.weight.copy_(key)
+        result.value.weight.copy_(value)
+        if self.use_bias:
+            query_bias, key_bias, value_bias = torch.split(
+                self.qkv_fused.bias, self.splits, dim=0
+            )
+            result.query.bias.copy_(query_bias)
+            result.key.bias.copy_(key_bias)
+            result.value.bias.copy_(value_bias)
+        return result
+
     def reset_parameters(self):
         nn.init.trunc_normal_(self.qkv_fused.weight, mean=0.0, std=0.02)
         if self.use_bias:
@@ -184,7 +206,39 @@ class FusedQKV(QKV):
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, emb_dim, emb_kq, emb_v, nheads, kvheads, p_dropout=None, use_bias=False, position_encoder: Optional[PositionEncoder] = None, fused: bool = True):
+    """
+    Performs multi-headed self- or cross-attention, with optional attention masking.
+    ...
+    Args
+    ----
+    emb_dim : int
+        Latent dimensionality of input and output tensors.
+    emb_kq : int
+        Latent dimensionality of each head in key and query projections (attention dimension).
+    emb_v : int
+        Latent dimensionality of each head in value projection (mixing dimension).
+    nheads : int
+        Number of attention heads.
+    p_dropout : float|None
+        Dropout probability. Must be in range [0,1]. If 0 or None, dropout will not be used.
+    use_bias : bool
+        Include bias terms in fully-connected sublayers?
+    fused: bool
+        if True, qkv weights will be fused, otherwise qkv weights will be unfused
+    """
+
+    def __init__(
+        self,
+        emb_dim,
+        emb_kq,
+        emb_v,
+        nheads,
+        kvheads,
+        p_dropout=None,
+        use_bias=False,
+        position_encoder: Optional[PositionEncoder] = None,
+        fused: bool = True,
+    ):
         super(MultiHeadAttention, self).__init__()
         self.nheads = nheads
         self.kvheads = kvheads
@@ -210,6 +264,7 @@ class MultiHeadAttention(nn.Module):
         if self.p_dropout:
             self.attn_dropout = nn.Dropout(self.p_dropout)
         self.position_encoder = position_encoder
+        # Avoiding graph breaks
         self.previous_flash: bool = torch.backends.cuda.flash_sdp_enabled()
         self.previous_mem_efficient: bool = (
             torch.backends.cuda.mem_efficient_sdp_enabled()
@@ -225,36 +280,76 @@ class MultiHeadAttention(nn.Module):
             elif isinstance(m, QKV):
                 m.reset_parameters()
 
-    def forward(self, q: torch.Tensor, k: Optional[torch.Tensor] = None, v: Optional[torch.Tensor] = None, mask: Optional[Tensor] = None, position_ids=None, attn_algorithm=None, past_key_value_state: Optional[Tuple[Tensor, Tensor]] = None, use_cache=False, is_self=True, is_causal_mask=False):
-        print("Entered MultiHeadAttention forward")
-        print(f"q.shape: {q.shape}, k.shape: {k.shape if k is not None else 'None'}, v.shape: {v.shape if v is not None else 'None'}")
-        
+    def to_tp(self, group: ProcessGroup) -> "TPMultiHeadAttention":
+        return TPMultiHeadAttention.import_module(self, group)
+
+    def forward(
+        self,
+        q: torch.Tensor,
+        k: Optional[torch.Tensor] = None,
+        v: Optional[torch.Tensor] = None,
+        mask: Optional[Tensor] = None,
+        position_ids=None,
+        attn_algorithm=None,
+        past_key_value_state: Optional[Tuple[Tensor, Tensor]] = None,
+        use_cache=False,
+        is_self=True,
+        is_causal_mask=False,
+    ):
+        """
+        past_key_value_state: tuple
+            the cache to be used in attention of the form (<self/cross>_key, <self/cross>_value)
+        position_ids: Optional[torch.LongTensor]
+            The position of each of the tokens encoded in q and k. Used for RoPE embeddings
+        use_cache: bool
+            if True, the kv states for self/cross attention will be saved, otherwise they will not be saved
+        is_self: bool
+            if True, this will perform self attention, otherwise this will perform cross attention. Note: This will
+            only be used in the case that use_cache=True. This may be removed in future
+
+        Returns
+        -------
+        tensor or tuple
+            If use_cache=False, only the hidden state will be returned as a tensor. If use_cache=True, a tuple will be
+            returned in the form (hidden_state, cache) where hidden_state is a tensor and cache is of the form specified
+            in past_key_value_state
+        """
+        # q, k, v: batch_size x seq_len x emb_dim
+        # mask: batch_size x seq_len x seq_len
         batch_size, q_len, _ = q.size()
-        is_prefilled = past_key_value_state is None or past_key_value_state[0].numel() == 0
 
-
-        print(f"is_prefilled: {is_prefilled}")
-
-        if is_self or is_prefilled:
-            print("Computing q_out, k_out, v_out")
+        # if this is self attention, we always recompute
+        # cross attention only gets computed when a cache does not exist
+        # if we dont have the cache yet, we need to compute
+        # d x (h x ds)
+        # b x kvlen x d
+        # b x kvlen x h x ds
+        # b x h x kvlen x ds
+        # todo: Cross attention (This always is true for now)
+        if is_self or past_key_value_state is None:
             q_out, k_out, v_out = self.in_proj(q, k, v)
 
+            # note: transposes will be moved in a later PR to fix dis-contiguous tensor issues
             queries = q_out.view(batch_size, q_len, self.nheads, self.emb_kq_per_head)
             keys = k_out.view(batch_size, q_len, self.kvheads, self.emb_kq_per_head)
             values = v_out.view(batch_size, q_len, self.kvheads, self.emb_v_per_head)
 
+            # You want to apply rotary embeddings pre-cache
             if self.position_encoder is not None:
-                print("Applying position encoder")
                 queries, keys = self.position_encoder.adjusted_qk(
                     queries, keys, position_ids, past_key_value_state, use_cache
                 )
 
-        queries = queries.transpose(2, 1)
-        keys = keys.transpose(2, 1)
-        values = values.transpose(2, 1)
+        queries = queries.transpose(2, 1)  # / (self.emb_kq_per_head**(1/4))
+        keys = keys.transpose(2, 1)  # / (self.emb_kq_per_head**(1/4))
+        values = values.transpose(2, 1)  # compatible with QK.T
 
-        if use_cache and not is_prefilled:
-            print("Using cache")
+        # if you want to use caching and past_key_value_state is not None meaning you have values in your cache
+        if (
+            use_cache
+            and past_key_value_state is not None
+            and past_key_value_state[0].numel() > 0
+        ):
             if is_self:
                 keys = torch.cat((past_key_value_state[0], keys), dim=2)
                 values = torch.cat((past_key_value_state[1], values), dim=2)
@@ -262,26 +357,34 @@ class MultiHeadAttention(nn.Module):
                 keys = past_key_value_state[0]
                 values = past_key_value_state[1]
 
+        # Merge rel pos bias and mask into single float mask
         if mask is not None:
-            while len(mask.size()) != 4:
+            # Our expected mask format is bs x q_len x k_len, so to make it broadcastable
+            # we need to create the nheads dimension
+            while len(mask.size()) != 4:  # expects bs (x nheads) x q_len x kv_len
                 mask = mask.unsqueeze(1)
 
         if self.position_encoder is not None:
-            attn_mask = self.position_encoder.adjusted_mask(
+            attn_mask: Optional[Tensor] = self.position_encoder.adjusted_mask(
                 mask, queries, keys, past_key_value_state, use_cache
             )
         else:
             attn_mask = mask
 
+        # Expand kv so black-box attn will work
         expansion = self.nheads // self.kvheads
+        # k/v: b h l d
         if expansion != 1:
             keys_e = keys.unsqueeze(2).expand(-1, -1, expansion, -1, -1).flatten(1, 2)
-            values_e = values.unsqueeze(2).expand(-1, -1, expansion, -1, -1).flatten(1, 2)
+            values_e = (
+                values.unsqueeze(2).expand(-1, -1, expansion, -1, -1).flatten(1, 2)
+            )
         else:
             keys_e = keys
             values_e = values
 
         if attn_algorithm:
+            # Pick which fused attn kernels will run.
             use_flash = attn_algorithm == "flash"
             use_mem_efficient = attn_algorithm == "mem"
             use_math = attn_algorithm == "math"
@@ -289,17 +392,6 @@ class MultiHeadAttention(nn.Module):
             torch.backends.cuda.enable_flash_sdp(use_flash)
             torch.backends.cuda.enable_mem_efficient_sdp(use_mem_efficient)
             torch.backends.cuda.enable_math_sdp(use_math)
-
-        #hardcode
-        # queries = torch.randn((1, 32, 48, 128), requires_grad=True)
-        # keys_e = torch.randn((1, 32, 48, 128), requires_grad=True)
-        # values_e = torch.randn((1, 32, 48, 128), requires_grad=True)
-        # attn_mask = None
-        # dropout_p = 0.0
-        # is_causal_mask = True
-
-        print(f"SDPA call params - queries: {queries.shape}, keys_e: {keys_e.shape}, values_e: {values_e.shape}, attn_mask: {attn_mask.shape if attn_mask is not None else 'None'}, dropout_p: {self.p_dropout if self.training else 0.0}, is_causal: {is_causal_mask}")
-
 
         attn = F.scaled_dot_product_attention(
             queries,
@@ -315,6 +407,10 @@ class MultiHeadAttention(nn.Module):
             torch.backends.cuda.enable_mem_efficient_sdp(self.previous_mem_efficient)
             torch.backends.cuda.enable_math_sdp(self.previous_math)
 
+        # attn: bs x seq_len x nheads*emb_v_per_head
+        # attn: b x h x qlen x ds
+        # attn after permute: b x qlen x h x ds
+        # b x qlen x (d)
         attn = (
             attn.transpose(2, 1)
             .contiguous()
@@ -322,11 +418,11 @@ class MultiHeadAttention(nn.Module):
         )
         out = self.dense(attn)
 
+        # if use_cache=True, we return the hidden_state as well as the kv cache
         if use_cache:
             return out, (keys, values)
         else:
             return out
-
 
 
 class TPMultiHeadAttention(MultiHeadAttention, TPModule):
